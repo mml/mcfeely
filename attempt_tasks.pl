a532a83a787a0834798f7fb616d91ce49124b894
Matt Liggett <mml@pobox.com>
1999-06-28 04:53:00 -0500

    added big comment

--- attempt_tasks.pl
+++ attempt_tasks.pl
@@ -13,10 +13,17 @@ sub attempt_tasks() {
 
     TASK: for ($i = 0; $i <= $#Tasks; ++$i) {
         $task = $Tasks[$i];
+
+        # Since the tasks are sorted by NEXT_TRY time, if this one is in the
+        # future, so are all the rest, so let's just cut to the chase.  In the
+        # process, we recreate @Tasks, but don't include any of the tasks we've
+        # just attempted.  When the results come back from the spawner, they'll
+        # either be removed from the action or reinserted into the queue.
         if ($task->[$TASK_NEXT_TRY] > $now) {
             @Tasks = @Tasks[$i..$#Tasks];
             last TASK;
         }
+
         next if $task->[$TASK_NDEPS] > 0;
         write_to_spawner $task->[$TASK_INO];
         $task->[$TASK_NEXT_TRY] = $now;
