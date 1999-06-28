900aa759760e8e4eb17df2f21f13c57d3e5d386e
Matt Liggett <mml@pobox.com>
1999-06-28 05:36:00 -0500

    changed log->plog

--- do_select.pl
+++ do_select.pl
@@ -1,9 +1,9 @@
 # wait on the trigger and other filehandles
 sub do_select() {
     sysopen(TRIGGER, 'trigger', O_RDONLY|O_NONBLOCK)
-        or log "Cannot open trigger: $!";
+        or plog "Cannot open trigger: $!";
     fcntl(TRIGGER, F_SETFL, fcntl(TRIGGER, F_GETFL, 0) & O_NONBLOCK)
-        or log "Cannot fcntl trigger: $!";
+        or plog "Cannot fcntl trigger: $!";
     select($rout=$rin, undef, undef, SLEEPYTIME);
     close(TRIGGER);
 }
