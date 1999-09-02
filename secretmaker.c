ca3e1b32a3dcf87e5f7f053075a90978aa6ca429
Adrian Hosey <alh@midnightowlsw.com>
1999-09-02 09:29:13 -0500

    Added secretmaker, to automatically generate keys for shared secrets.

---
+++ secretmaker.c
@@ -0,0 +1,37 @@
+/** $Id: secretmaker.c,v 1.1 1999/09/02 14:29:13 ahosey Exp $
+ *  Create a 512-bit (64 byte) secret appropriate for use as a McFeely
+ *  shared secret. The secret is written into "newsecret" in the
+ *  current directory.
+ **/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+  int fd, random_byte, x;
+
+  srandom( (int)time(NULL) );
+
+  if ((fd = open("newsecret", O_WRONLY|O_CREAT|O_EXCL, 00600)) < 0) {
+    perror("cannot create \"newsecret\"");
+    return 2;
+  }
+  for (x = 0; x < 64; x++) {  
+    random_byte = 1 + (int) (256.0 * random() / (RAND_MAX + 1.0));
+    if (write(fd, &random_byte, 1) != 1) {
+      perror("problem writing secret value to file");
+      return 2;
+    }
+  }
+  if (write(fd, "\n", 1) != 1) {
+    perror("problem writing newline into the file?? ");
+    return 2;
+  }
+  close(fd);
+
+  return 0;
+}
