40e24cb812800edb0194c87104676b9c742bdc9d
Adrian Hosey <alh@midnightowlsw.com>
2000-02-09 16:32:19 -0500

    Changed snooze to wake every 60 seconds and make sure its parent is still around. If not then exit. Moved snooze binary into the mcfeely-server package since that's the one that really needs it.

--- snooze.c
+++ snooze.c
@@ -1,8 +1,17 @@
 #include <unistd.h>
 
-void main(void)
+int main()
 {
-	pause(); /* suspend self until killed by almost any signal
-                 ** see signal(7)
-                 */
+  /* Wake every 60 seconds and check for our parent. If we were
+   * inherited by init our parent is gone so we can go too. We have to
+   * do this because sshd puts us in our own session so we don't
+   * automatically respond to the exit of our parent.  
+   */
+  for(;;) {
+    sleep(60);
+    if (getppid() == 1)      
+      return 0;
+  }
+  /* We fell out of our loop! Shouldn't happen. */
+  return 1;
 }
