947ace76fc4373d33cdf940b775c11cda59deedd
Chris Dent <cdent@users.noreply.github.com>
2000-06-21 19:58:37 -0500

    Fixed a big in mcfeely-ttpc.c where the error check for knsfwrite on the writing of the task to the socket was not checking for -1 (an error condition) instead it was checking for writing of one bite. It was hoped that this would help the situation with stalled out tasks and jobs that don't get reported back but that doesn't look like that is the case.
    Doing some tests where I blow just shitloads of tasks straight
    from the command line into mcfeely-ttpc seem to indicate that
    everything is making it's way back to mcfeely-ttpc, except in the
    case where there are too many open files. I don't think that is
    the problem here, but I'm not sure.

--- mcfeely-ttpc.c
+++ mcfeely-ttpc.c
@@ -42,25 +42,6 @@ void hard(char *m, int l) { write(1, m, l); exit(100); }
 void soft_write(void) { soft("write failure", 13); }
 void soft_read(void)  { soft("read failure",  12); }
 
-/* given a taskid as a string, open the file on fd */
-int task_open(taskid)
-char *taskid;
-{
-    char *it;
-    int len;
-    int fd;
-
-    /* "queue/task/TASKID" -- TASKID+12 */
-    len = strlen(taskid) + 12;
-    it = (char *)malloc(len);
-    if (! it) return 0;
-
-    snprintf(it, len, "queue/task/%s", taskid);
-    fd = open(it, O_RDONLY);
-    free(it);
-    return fd;
-}
-
 int
 secret_open(fnam)
 char *fnam;
@@ -134,7 +115,12 @@ char *argv[];
     if (write(s, &tasknum, 4) != 4)   soft_write();
     if (write(s, "\0\0\0\0", 4) != 4) soft_write();
     if (write(s, "\0\0\0\0", 4) != 4) soft_write();
-    if (knsfwrite(s, 0) == 1)       soft_write();
+
+    /* this right here deserves a comment because althought
+       it isn't immediately clear, this is the crux of the
+       biscuit, this takes the entire contents of STDIN and
+       writes it to the socket */
+    if (knsfwrite(s, 0) == -1)       soft_write();
 
     /* recv: response */
     if (read(s, &code, 1) != 1)  soft_read();
